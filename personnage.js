c54d3f771eea54949eabe3d5d90728cad941d054
Icez <pierrekeller75@gmail.com>
2020-04-21 13:48:51 +0200

    Finalisation des mouvements

--- personnage.js
+++ personnage.js
@@ -1,34 +1,35 @@
-
-
-
-const SCALE = 1
-const WIDTH = 50
-const HEIGHT = 37
+// Valeurs Globales constantes
+const SCALE = 1     // Taille du sprite 
+const WIDTH = 50  // Largeur du Sprite
+const HEIGHT = 37   // Hauteur  du Sprite
 const SCALED_WIDTH = SCALE * WIDTH
 const SCALED_HEIGHT = SCALE * HEIGHT
-const CYCLE_LOOP = [0, 1, 2, 3]
-const CYCLE_LOOP_JUMP = [0, 1, 2, 3, 4, 5, 6, 7]
-const CYCLE_LOOP_SLIDE = [0, 1, 2, 3, 4]
-const FACING_DOWN = 4
-const FACING_UP = 3
-const FACING_LEFT = 1
-const FACING_RIGHT = 0
-const STAND_STILL = 2
-const FRAME_LIMIT = 12
-const MOVEMENT_SPEED = 1
+const CYCLE_LOOP = [0, 1, 2, 3] // Tableau pour 4 éléements dans le sprite
+const CYCLE_LOOP_JUMP = [0, 1, 2, 3, 4]  // Tableau pour 8 éléements dans le sprite  /!\/!\/!\ Seulement 5 sprites sont prises en compte
+const CYCLE_LOOP_SLIDE = [0, 1, 2, 3, 4]  // Tableau pour 5 éléements dans le sprite
+const FACING_RIGHT = 0  // Action droite
+const FACING_LEFT = 1 // Action courir gauche
+const STAND_STILL = 2   // Action IDLE
+const FACING_UP_RIGHT = 3   // Action Sauter à gauche
+const FACING_UP_LEFT = 4
+const FACING_DOWN_RIGHT = 5 
+const FACING_DOWN_LEFT = 6  
+const FRAME_LIMIT = 12  // Vitesse de changement entre chaque frame
+const MOVEMENT_SPEED = 1 // Vitesse de déplacement
 
-let hasSlided = false
-let isSliding = false
-let compteurSlide = 0
-let hasJumped = false
-let msJump = 0
-let canvas = document.querySelector('canvas')
-let ctx = canvas.getContext('2d')
+let jumpingLeft = false
+let isSlidingLeft = false  
+let hasSlided = false // à glisser
+let isSliding = false // Glisse 
+let hasJumped = false // à sauter
+let msJump = 0  // Vitesse de saut
+let canvas = document.querySelector('canvas')  
+let ctx = canvas.getContext('2d') 
 let keyPresses = {}
-let currentDirection = FACING_DOWN
-let currentLoopIndex = 0
-let frameCount = 0
-let positionX = 0
+let currentDirection = FACING_DOWN_RIGHT 
+let currentLoopIndex = 0    // à mettre {...}
+let frameCount = 0        // Compte les frames (jusqu'à 12 sur ce code)
+let positionX = 0      
 let positionY = canvas.height - HEIGHT
 let img = new Image()
 
@@ -43,13 +44,13 @@ function keyUpListener(event) {
 }
 
 function loadImage() {
-  img.src = 'runAnimation.png'
-  img.onload = function() {
+  img.src = 'runAnimation.png'      // <--  source de l'image 
+    img.onload = function() {
     window.requestAnimationFrame(gameLoop)
   }
 }
 
-function drawFrame(frameX, frameY, canvasX, canvasY) {
+function drawFrame(frameX, frameY, canvasX, canvasY) {        // Ne pas toucher, fonction pour afficher le perso
   ctx.drawImage(img,
                 frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
                 canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT)
@@ -57,23 +58,22 @@ function drawFrame(frameX, frameY, canvasX, canvasY) {
 
 loadImage()
 
-function gameLoop() {
+function gameLoop() {                                 // Fonction principale 
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   let hasMoved = false
-  let compteurSlideBool = false
 
-  positionY -= msJump
+  positionY -= msJump         // {...} Permet d'arreter le saut 
   if ( positionY > canvas.height - HEIGHT && hasJumped == true) {
       hasJumped = false
       msJump = 0
       currentLoopIndex = 0
   }
-  if (msJump !== 0 ) {
+  if (msJump !== 0 ) {    // Ajout de la gravité 
     msJump *= .99
     msJump -= .1
   }
 
-  if (keyPresses.z && !hasJumped) {
+  if (keyPresses.z && !hasJumped && keyPresses.d ) {   // Empeche de slider après un saut ou un slide de 1.5secondes
     currentLoopIndex = 0
     msJump = 3.5
     hasJumped = true
@@ -82,27 +82,44 @@ function gameLoop() {
     setTimeout(() => {
       hasSlided = false
     }, 1500);
-   } 
+   } else if (keyPresses.z && !hasJumped && keyPresses.q){
+    currentLoopIndex = 0
+    msJump = 3.5
+    hasJumped = true
+    isSliding = false
+    hasSlided = true
+    jumpingLeft = true
+    setTimeout(() => {
+      hasSlided = false
+    }, 1500);
+   }
 
-  if (keyPresses.q && !isSliding) {
+   
+  if (keyPresses.q && !isSliding) {               // Courir vers la gauche
     moveCharacter(-MOVEMENT_SPEED, 0, FACING_LEFT)
     hasMoved = true
   } else if (keyPresses.d && !isSliding) {
-    moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT)
+    moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT)        // Courir vers la droite
     hasMoved = true
   }
 
-  if (keyPresses.d && keyPresses.s && !hasSlided && !isSliding) {
-    moveCharacter(0, 0, FACING_DOWN)
+  if (keyPresses.d && keyPresses.s && !hasSlided && !isSliding) {       // Permetde slide vers la droite si le perso ne slide pas et n'a pas déjà slider
+    moveCharacter(0, 0, FACING_DOWN_RIGHT)
     currentLoopIndex = 0
     isSliding = true
+  } else if (keyPresses.q && keyPresses.s && !hasSlided && !isSliding) {       
+    moveCharacter(0, 0, FACING_DOWN_LEFT)
+    currentLoopIndex = 0
+    isSliding = true
+    isSlidingLeft = true
   }
 
-  if (!hasMoved) {
+
+  if (!hasMoved) {                      // Animation IDLE 
     moveCharacter(0, 0, STAND_STILL)
   }
 
-  if (!hasJumped && !isSliding) {
+  if (!hasJumped && !isSliding) {         // Cycle des frames de base 
     frameCount++
     if (frameCount >= FRAME_LIMIT) {
       frameCount = 0
@@ -111,20 +128,30 @@ function gameLoop() {
         currentLoopIndex = 0
       }
     }
-  } else if (hasJumped) {
-    currentDirection = FACING_UP
+  } else if (hasJumped) {         // Cycle des frames pour le saut 
+    if (jumpingLeft == false) {
+      currentDirection = FACING_UP_RIGHT
+    } else if (jumpingLeft == true) {
+      currentDirection = FACING_UP_LEFT
+    }
     frameCount++
     if (frameCount >= FRAME_LIMIT) {
       frameCount = 0
       currentLoopIndex++
       if (currentLoopIndex >= CYCLE_LOOP_JUMP.length) {
         currentLoopIndex = 0
+        jumpingLeft = false
       }
     }
-  } else if (isSliding) {
-      moveCharacter(MOVEMENT_SPEED, 0, FACING_DOWN)
-      currentDirection = FACING_DOWN
-      frameCount++
+  } else if (isSliding) {                               // Cycle des frames pour le slide     
+      if (isSlidingLeft == false) {
+      moveCharacter(MOVEMENT_SPEED, 0, FACING_DOWN_RIGHT)
+      currentDirection = FACING_DOWN_RIGHT 
+      } else if (isSlidingLeft == true) {
+        moveCharacter(-MOVEMENT_SPEED, 0, FACING_UP_LEFT)
+        currentDirection = FACING_DOWN_LEFT 
+        }
+        frameCount++
       if (frameCount >= FRAME_LIMIT) {
         frameCount = 0
         currentLoopIndex++
@@ -132,39 +159,30 @@ function gameLoop() {
             currentLoopIndex = 0
             hasSlided = true
             isSliding = false
+            isSlidingLeft = false
             setTimeout(() => {
               hasSlided = false
             }, 1000);
         }
       }
     }
- 
-
-
-  //  if (compteurSlideBool == true) {
-  //   compteurSlide++
-  //   console.log(compteurSlide,'salope')
-  //     if (compteurSlide == 8) {
-  //     }
-  //   }
-
 
   if (positionY > canvas.height - HEIGHT) {
       positionY = canvas.height - HEIGHT
   }
 
-  if (!hasJumped && !isSliding) {
-  drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY)
+  if (!hasJumped && !isSliding) {   
+  drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY)           // Appelle de Drawframe pour l'affichage des frames de base
   } else if (hasJumped) { 
-    drawFrame(CYCLE_LOOP_JUMP[currentLoopIndex], currentDirection, positionX, positionY)
+    drawFrame(CYCLE_LOOP_JUMP[currentLoopIndex], currentDirection, positionX, positionY)      // Appelle de Drawframe pour l'affichage des frames de saut
   } else{
-    drawFrame(CYCLE_LOOP_SLIDE[currentLoopIndex], currentDirection, positionX, positionY)
+    drawFrame(CYCLE_LOOP_SLIDE[currentLoopIndex], currentDirection, positionX, positionY)     // Appelle de Drawframe pour l'affichage des frames de slide
   }
   window.requestAnimationFrame(gameLoop)
-  // console.log(currentLoopIndex)
+  // console.log(currentLoopIndex)  // Pour vérifier la valeur actuelle de la frame en cas de problème
 }
 
-function moveCharacter(deltaX, deltaY, direction) {
+function moveCharacter(deltaX, deltaY, direction) {                                    // Déplacement du personnage
   if (positionX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
     positionX += deltaX
   }
