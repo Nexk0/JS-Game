91a2933aac947be245af108cde4da5e5480b91b2
Icez <pierrekeller75@gmail.com>
2020-04-23 17:30:07 +0200

    Niveau Suivant Collision

--- README.md
+++ README.md
@@ -2,17 +2,14 @@
 🔨Jeu en js et HTML pour la 3e semaine intensive d'HETIC 🔨
 
 
-We are working...
-
+We are working
 
 21/04/2020
 
-Début des Sprite de notre personnage...
 Notre player se déplace, à une physique cohérente.
-On attaque la modélisation de la map.
+On attaque la modélisation de la map
 
 lets go... 🚧🔋
-
 
 22/04/2020
 
@@ -20,26 +17,4 @@ lets go... 🚧🔋
 
 ♻︎résolutions de bugs avec le player (fix du background aussi)♻︎
 
-Création de la page d'accueil et du spawn !
-Un dialogue est mis en place pour introduire l'univers et l'histoire en CSS/JS
 
-
-Detection de Bug : Avec MAJ activé le perso ne bouge pas
-                  L'ambiance sonore ne se lance pas
-                  Incompatibilité sur Firefox...
-
-Lucas Stankoff a un problème avec son Git, impossible de commit.
-
-23/04/2020
-
-Apparition de nouveaux sons fait maison (épée, mort et saut !)
-♻︎ Réglage de certain bug ♻︎
-Les pièces font enfin leur apparition.
-Les pages sont reliées entre elles !
-Commit toujours impossible de la part de Lucas tant pis !
-
-Objectif de cette nuit : Terminer le niveau 2.
-
-24/04/2020
-
-On vous laisse entrer dans notre univers désormais.
--- dev/scripts/personnage.js
+++ dev/scripts/personnage.js
@@ -1,26 +1,32 @@
 // Valeurs Globales constantes
-const SCALE = 1 // Taille du sprite
+const SCALE = 1 // Taille du sprite 
 const SCALE_FIRE = 0.3
 const SCALE_GOLD = 1
-const WIDTH = 50 // Largeur du Sprite du personnage principale
+const SCALE_NV_SUIVANT = 1
+const WIDTH = 50 // Largeur du Sprite du personnage principale 
 const HEIGHT = 37 // Hauteur  du Sprite
 const FIRE_WIDTH = 125 // Largeur du sprite de feu
 const FIRE_HEIGHT = 208 // Hauteur du sprite de feu
 const GOLD_WIDTH = 32   // Largeur du sprite de feu
 const GOLD_HEIGHT = 32  // Hauteur du sprite de feu
+const NV_WIDTH = 32
+const NV_HEIGHT = 32
 const SCALED_WIDTH = SCALE * WIDTH
 const SCALED_HEIGHT = SCALE * HEIGHT
 const SCALED_WIDTH_FIRE = SCALE_FIRE * FIRE_WIDTH
 const SCALED_HEIGHT_FIRE = SCALE_FIRE * FIRE_HEIGHT
 const SCALED_WIDTH_GOLD = SCALE_GOLD * GOLD_WIDTH
 const SCALED_HEIGHT_GOLD = SCALE_GOLD * GOLD_HEIGHT
+const SCALED_WIDTH_NV_SUIVANT = SCALE_NV_SUIVANT * NV_WIDTH
+const SCALED_HEIGHT_NV_SUIVANT = SCALE_NV_SUIVANT * NV_HEIGHT
 const CYCLE_LOOP = [0, 1, 2, 3] // Tableau pour 4 éléements dans le sprite
 const CYCLE_LOOP_JUMP = [0, 1, 2, 3, 4] // Tableau pour 8 éléements dans le sprite  /!\/!\/!\ Seulement 5 sprites sont prises en compte
 const CYCLE_LOOP_SLIDE = [0, 1, 2, 3, 4] // Tableau pour 5 éléements dans le sprite
 const CYCLE_LOOP_PRIMARTY_ATTACK = [0, 1, 2, 3, 4, 5, 6, 7] // Tableau pour attaquer à droite
 const CYCLE_LOOP_PRIMARTY_ATTACK_LEFT = [7, 6, 5, 4, 3, 2, 1, 0] // reverse du tableau pour attaquer à gauche
 const CYCLE_LOOP_FIRE = [5, 6, 7, 8, 9] // Tableau pour le cycle du feu
-const CYCLE_LOOP_GOLD = [0, 1, 2, 3, 4, 5, 6, 7, 8] // Tableau pour le cycle des golds
+const CYCLE_LOOP_GOLD = [0, 1, 2, 3, 4, 5, 6, 7, 8] 
+const CYCLE_LOOP_NV = [0, 1, 2, 3, 4, 5, 6, 7, 8] // Tableau pour le cycle des golds
 const FACING_RIGHT = 0 // Choix de la ligne du sprite, chiffre = lien, courir à droite
 const FACING_LEFT = 1 // Action courir gauche
 const STAND_STILL = 2 // Action IDLE
@@ -36,19 +42,21 @@ const DECAL_CHAR_X = 15
 const CHARWIDTH = 20
 const LIFE = 3          // Vie
 
+let currentNvLoopIndex = 0
+let frameCountNv = 0
 let frameCountGold = 0      // Comptage de chaque frame pour les Golds
 let currentGoldLoopIndex = 0        // Loop index gold
 let frameCountFire = 0          // Comptage de chaque frame du feu
 let currentFireLoopIndex = 0        // Loop index du feu
 let charX
-let charY
+let charY 
 let lookingLeft = false     // Regarde à gauche
 let lookingRight = false       // Regarde à droite
 let frameLimit = 12         // Vitesse de changement entre chaque frame
 let hasAttacked = false         // A attaquer
 let primaryAttack = false       // Attaque de base
 let hasSlided = false // à glisser
-let isSliding = false // Glisse
+let isSliding = false // Glisse 
 let hasJumped = false // à sauter
 let onGround = true     // Est sur le sol
 let msJump = 0 // Vitesse de saut
@@ -63,8 +71,9 @@ let positionY = 134     // Personnage Y du personnage
 let img = new Image()      // Img du main characters
 let imgFire = new Image()      // Img du feu
 let imgGold = new Image()       // Img du gold
-let vy = 0
-let vx = 0
+let imgNv = new Image()
+let vy = 0;
+let vx = 0;
 let audioMusic = new Audio()        // Musique d'ambiance
 audioMusic.loop = true
 audioMusic.volume = 0.3
@@ -94,9 +103,8 @@ class Fire{                 // Orienté objet pôur la création du feu
         this.x = x
         this.y = y
     }
-    draw() {
-        ctx.fillStyle = 'rgba(255, 165, 0, 0)'
-        drawFrameFire(CYCLE_LOOP_FIRE[currentFireLoopIndex], 0, this.x, this.y)
+    draw() { 
+        drawFrameFire(CYCLE_LOOP_FIRE[currentFireLoopIndex], 0, this.x, this.y)       
     }
 }
 class Gold{                 // Orienté objet pôur la création des golds
@@ -105,9 +113,17 @@ class Gold{                 // Orienté objet pôur la création des golds
         this.y = y
         this.pickupGold = false
     }
-    draw() {
-        ctx.fillStyle = 'rgba(255, 165, 0, 0)'
-        drawFrameGold(CYCLE_LOOP_GOLD[currentGoldLoopIndex], 0, this.x, this.y)
+    draw() { 
+        drawFrameGold(CYCLE_LOOP_GOLD[currentGoldLoopIndex], 0, this.x, this.y)       
+    }
+}
+class NiveauSuivant{                 // Orienté objet pôur la création des golds
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+    draw() { 
+        drawFrameNiveauSuivant(CYCLE_LOOP_NV[currentNvLoopIndex], 0, this.x, this.y)       
     }
 }
 
@@ -115,21 +131,24 @@ class Gold{                 // Orienté objet pôur la création des golds
 *   Positions des objets
 */
 
+let niveauSuivants = []
+niveauSuivants.push(new NiveauSuivant(0, 360))
+
 let golds = []   // Ajout des Golds
-golds.push(new Gold(595, 110))
-golds.push(new Gold(635, 180))
-golds.push(new Gold(685, 240))
+golds.push(new Gold(595, 110))  
+golds.push(new Gold(635, 180)) 
+golds.push(new Gold(685, 240))                     
 
 let fires = []   // Ajout du feu
-fires.push(new Fire(262, 174))
+fires.push(new Fire(262, 174))                      
 fires.push(new Fire(262, 405))
 
 let plateforms = []     // Ajout de plateformes
-plateforms.push(new Plateform(0, 171, 266, 73))
-plateforms.push(new Plateform(265, 220, 30, 24))
-plateforms.push(new Plateform(295, 171, 250, 73))
-plateforms.push(new Plateform(0, 405, 266, 60))
-plateforms.push(new Plateform(296, 405, 510, 60))
+plateforms.push(new Plateform(0, 171, 269, 73))
+plateforms.push(new Plateform(269, 220, 26, 24))
+plateforms.push(new Plateform(293, 171, 250, 73))
+plateforms.push(new Plateform(0, 405, 269, 60))
+plateforms.push(new Plateform(294, 405, 510, 60))
 plateforms.push(new Plateform(800, 205, 100, 250))
 plateforms.push(new Plateform(0, 85, 70, 90))
 
@@ -157,9 +176,6 @@ document.onclick = function () {
         currentLoopIndex = 0
         primaryAttack = true
         hasAttacked = true
-        let swordSoundRight = new Audio()
-        swordSoundRight.src = "./dev/assets/sounds/sword.wav"
-        swordSoundRight.play()
         setTimeout(() => {
             hasAttacked = false
         }, 700)
@@ -168,9 +184,6 @@ document.onclick = function () {
         currentLoopIndex = 0
         primaryAttack = true
         hasAttacked = true
-        let swordSoundLeft = new Audio()
-        swordSoundLeft.src = "./dev/assets/sounds/sword.wav"
-        swordSoundLeft.play()
         setTimeout(() => {
             hasAttacked = false
         }, 700)
@@ -207,7 +220,7 @@ function drawFrameFire(frameX, frameY, canvasX, canvasY) {
     ctx.drawImage(imgFire,
         frameX * FIRE_WIDTH, frameY * FIRE_HEIGHT, FIRE_WIDTH, FIRE_HEIGHT,
         canvasX, canvasY, SCALED_WIDTH_FIRE, SCALED_HEIGHT_FIRE)
-
+        
 }
 
 
@@ -222,10 +235,23 @@ function drawFrameGold(frameX, frameY, canvasX, canvasY) {
     ctx.drawImage(imgGold,
         frameX * GOLD_WIDTH, frameY * GOLD_HEIGHT, GOLD_WIDTH, GOLD_HEIGHT,
         canvasX, canvasY, SCALED_WIDTH_GOLD, SCALED_HEIGHT_GOLD)
-
+        
 }
 
+function loadImageNiveauSuivant() {
+    imgNv.src = './dev/assets/Sprites/NiveauSuivant/NiveauSuivant.png' // source de l'image du gold
+    imgNv.onload = function () {
+    }
+}
 
+function drawFrameNiveauSuivant(frameX, frameY, canvasX, canvasY) {
+    ctx.drawImage(imgNv,
+        frameX * NV_WIDTH, frameY * NV_HEIGHT, NV_WIDTH, NV_HEIGHT,
+        canvasX, canvasY, SCALED_WIDTH_NV_SUIVANT, SCALED_HEIGHT_NV_SUIVANT)
+        
+}
+
+loadImageNiveauSuivant()
 loadImageGold()
 loadImageFire()
 loadImage()
@@ -242,10 +268,10 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
     }
 
     charX = positionX + DECAL_CHAR_X                // Création de la plateform
-    charY = positionY
+    charY = positionY 
     plateforms.forEach(function (plateform){
         plateform.draw()
-    })
+    }) 
 
     let hasMoved = false
 
@@ -263,15 +289,15 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
 
 
     if (onGround) msJump = 0
-    else if (!onGround) {
+    else if (!onGround) { 
         msJump = Math.floor(msJump*10)/10
         msJump -= .1
     }
 
-     vy = msJump
-    vx = 0
+     vy = msJump;
+    vx = 0; 
 
-    // KEYPRESS //
+    // KEYPRESS // 
 
     if (keyPresses.z && !hasJumped && keyPresses.d) { // Empeche de slider après un saut ou un slide de 1.5secondes
         currentLoopIndex = 0
@@ -284,7 +310,7 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
         jumpSoundRight.play()
         setTimeout(() => {
             hasSlided = false
-        }, 1500)
+        }, 1500);
     } else if (keyPresses.z && !hasJumped && keyPresses.q) {
         currentLoopIndex = 0
         msJump = 3.5
@@ -297,7 +323,7 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
         jumpSoundLeft.play()
         setTimeout(() => {
             hasSlided = false
-        }, 1500)
+        }, 1500);
     }
 
 
@@ -344,16 +370,19 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
     if (checkFireCollision(charX, charY)) {
         positionX = 60
         positionY = 130
-        let fireSound = new Audio()
-        fireSound.src = "./dev/assets/sounds/fire.wav"
-        fireSound.play()
+    }
+
+    if (checkNvSuivantCollision(charX, charY)){
+        document.location.href = './Page_Accueil/index.html'
     }
 
     checkGoldCollision(charX, charY)
+    checkNvSuivantCollision(charX, charY)
+    
 
     // CYCLES //
 
-    if (!hasJumped && !isSliding && !primaryAttack) { // Cycle des frames de base
+    if (!hasJumped && !isSliding && !primaryAttack) { // Cycle des frames de base 
         frameCount++
         if (frameCount >= frameLimit) {
             frameCount = 0
@@ -362,10 +391,10 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
                 currentLoopIndex = 0
             }
         }
-    } else if (hasJumped) { // Cycle des frames pour le saut
+    } else if (hasJumped) { // Cycle des frames pour le saut 
         if (lookingRight) {
             currentDirection = FACING_UP_RIGHT
-        }
+        } 
         if (lookingLeft) {
             currentDirection = FACING_UP_LEFT
         }
@@ -377,7 +406,7 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
                 currentLoopIndex = 0
             }
         }
-    } else if (isSliding) { // Cycle des frames pour le slide
+    } else if (isSliding) { // Cycle des frames pour le slide     
         if (lookingRight) {
             if(!checkPlayerCollision(MOVEMENT_SPEED, 0)) moveCharacter(MOVEMENT_SPEED, 0, FACING_DOWN_RIGHT)
             else moveCharacter(0, 0, FACING_DOWN_RIGHT)
@@ -407,7 +436,7 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
         currentDirection = FACING_PRIMARY_ATTACK
         }   else if (lookingLeft) {
             currentDirection = FACING_PRIMARY_ATTACK_LEFT
-        }
+        } 
         if (frameCount >= frameLimit) {
             frameCount = 0
             currentLoopIndex++
@@ -418,8 +447,8 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
         }
     }
 
-
-    frameCountFire++    // Cycle des frames du feu
+                             
+    frameCountFire++    // Cycle des frames du feu 
     if (frameCountFire >= frameLimit) {
         frameCountFire = 0
         currentFireLoopIndex++
@@ -428,7 +457,7 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
         }
     }
 
-    frameCountGold++    // Cycle des frames du gold
+    frameCountGold++    // Cycle des frames du gold 
     if (frameCountGold >= frameLimit) {
         frameCountGold = 0
         currentGoldLoopIndex++
@@ -437,10 +466,18 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
         }
     }
 
+    frameCountNv++    // Cycle des frames du nvSuivant
+    if (frameCountNv >= frameLimit) {
+        frameCountNv = 0
+        currentNvLoopIndex++
+        if (currentNvLoopIndex >= CYCLE_LOOP_NV.length) {
+            currentNvLoopIndex = 0
+        }
+    }
+
     if (charY > canvas.height - HEIGHT) {
         charY = canvas.height - HEIGHT
     }
-
 
     /* plateforms.forEach(function (plateform){                                      // Gestion de la hitbox de la plateform
         if(charX < plateform.x + plateform.width &&
@@ -449,7 +486,7 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
             HEIGHT + charY > plateform.y) {
         }
     })  */
-
+    
     if (!hasJumped && !isSliding && !primaryAttack) {
         drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY) // Appelle de Drawframe pour l'affichage des frames de base
     } else if (hasJumped) {
@@ -461,24 +498,27 @@ function gameLoop() {               // Fonction principale s'occupe des dessins,
     } else if (lookingLeft) {
         drawFrame(CYCLE_LOOP_PRIMARTY_ATTACK_LEFT[currentLoopIndex], currentDirection, positionX, positionY)    // Appelle de Drawframe pour l'affichage des frames d'attaque a gauche'
     }
-
-    fires.forEach(function (fire){      //Permet de dessiner le feu
-        fire.draw()
+    
+    fires.forEach(function (fire){      //Permet de dessiner le feu  
+        fire.draw()  
     })
-    golds.forEach(function (gold){      //Permet de dessiner le feu
+    golds.forEach(function (gold){      //Permet de dessiner le feu  
         if (!gold.pickupGold)
-        gold.draw()
+        gold.draw()  
+    })
+    niveauSuivants.forEach(function (niveauSuivant){      //Permet de dessiner le feu  
+        niveauSuivant.draw()  
     })
     window.requestAnimationFrame(gameLoop)
     // console.log(currentLoopIndex) // Pour vérifier la valeur actuelle de la frame en cas de problème
 }
 
-function checkGround(y) {
+function checkGround(y) {       
     testX = charX
     testY = charY + y
     let ground = false
     if(testX < 0 || testX + CHARWIDTH > canvas.width || testY < 0 || testY + HEIGHT > canvas.height) {
-        //if(testY + HEIGHT > canvas.height)
+        //if(testY + HEIGHT > canvas.height) 
         ground = true
     }
     plateforms.forEach(function (plateform){
@@ -486,7 +526,7 @@ function checkGround(y) {
             testX + CHARWIDTH > plateform.x &&
             testY < plateform.y + plateform.height &&
             HEIGHT + testY > plateform.y) {
-                //if(testY + HEIGHT > plateform.y)
+                //if(testY + HEIGHT > plateform.y) 
                 ground = true
         }
     })
@@ -535,6 +575,20 @@ function checkGoldCollision(x, y) {     // Pour détecter la collision avec les 
         }
     })
 }
+
+function checkNvSuivantCollision(x, y) {     // Pour détecter la collision l'objet niveau suivant
+    let niveauSuivantCollision = false
+    niveauSuivants.forEach(function (niveauSuivant){
+        if(x < niveauSuivant.x + NV_WIDTH * SCALE_NV_SUIVANT &&
+            x + CHARWIDTH > niveauSuivant.x &&
+            y < niveauSuivant.y + NV_HEIGHT * SCALE_NV_SUIVANT &&
+            HEIGHT + y > niveauSuivant.y) {
+            niveauSuivantCollision = true
+        }
+    })
+    return niveauSuivantCollision
+}
+
 
 function moveCharacter(deltaX, deltaY, direction) { // Déplacement du personnage && gestion de la hitbox du personnage (plus petite que la width du sprite)
     if (charX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
